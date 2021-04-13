191c5af9d4738c2a1303d7dffb89f67eb0140516
Chris Winkler <procy.dev@gmail.com>
2021-04-13 15:51:17 -0700

    Refactored cards to have more granular components

--- src/Components/CountryPicker/CountryPicker.jsx
+++ src/Components/CountryPicker/CountryPicker.jsx
@@ -15,7 +15,7 @@ const CountryPicker = ({ handleCountryChange }) => {
     return (
         <FormControl className={styles.formControl}>
             <NativeSelect default="" onChange={(e) => handleCountryChange(e.target.value)}>
-                <option value="global">Global</option>
+                <option value="">Global</option>
                 {countries.map((country, i) => <option key={i} value={country}>{country}</option>)}
             </NativeSelect>
         </FormControl>
